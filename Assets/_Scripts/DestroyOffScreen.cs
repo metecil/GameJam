8cc974fa464ae00255b0b88ea2e5d3986aba9db8
TylerSchaus <tylerdschaus@shaw.ca>
2025-03-21 15:21:17 -0700

    Player can shoot and destroy asteroids

--- Assets/_Scripts/DestroyOffScreen.cs
+++ Assets/_Scripts/DestroyOffScreen.cs
@@ -2,32 +2,8 @@ using UnityEngine;
 
 public class DestroyOffScreen : MonoBehaviour
 {
-    private float screenWidth;
-    private float screenHeight;
-    private Camera mainCamera;
-    private bool isVisible = false; 
-
-    private void Start()
+    private void OnBecameInvisible()
     {
-        mainCamera = Camera.main;
-        screenHeight = mainCamera.orthographicSize;
-        screenWidth = screenHeight * mainCamera.aspect;
-        Invoke(nameof(EnableVisibilityCheck), 1f);
-    }
-
-    private void EnableVisibilityCheck()
-    {
-        isVisible = true;
-    }
-
-    private void Update()
-    {
-        if (!isVisible) return; 
-
-        Vector3 viewportPos = mainCamera.WorldToViewportPoint(transform.position);
-        if (viewportPos.x < -0.1f || viewportPos.x > 1.1f || viewportPos.y < -0.1f || viewportPos.y > 1.1f)
-        {
-            Destroy(gameObject);
-        }
+        Destroy(gameObject); 
     }
 }
--- Assets/_Scripts/LaserController.cs
+++ Assets/_Scripts/LaserController.cs
@@ -15,8 +15,8 @@ public class LaserController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player")) return;
 
-    
 
+        Destroy(other.gameObject);
         Destroy(gameObject);
     }
 }
