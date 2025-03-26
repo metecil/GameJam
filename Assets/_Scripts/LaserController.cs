ff76923c9c33c435fa2861bf9493705c58eb7b3b
mete2001 <metecil09@gmail.com>
2025-03-25 20:51:17 -0700

    booster and screen wrapping for laser added

--- Assets/_Scripts/LaserController.cs
+++ Assets/_Scripts/LaserController.cs
@@ -4,22 +4,46 @@ public class LaserController : MonoBehaviour
 {
     [SerializeField] private float speed;
 
-    private void Update()
+    private float totalDistanceTraveled = 0f;
+    private Camera mainCamera;
+    private float maxDistance;
+
+    private void Start()
     {
-        transform.Translate(Vector3.up * (speed * Time.deltaTime));
+        mainCamera = Camera.main;
+        
+        // Calculate screen width and height in world units for an orthographic camera.
+        float screenHeight = 2f * mainCamera.orthographicSize;
+        float screenWidth = screenHeight * mainCamera.aspect;
+        maxDistance = Mathf.Sqrt(screenWidth * screenWidth + screenHeight * screenHeight);
+        
     }
 
-    private void OnBecameInvisible() => Destroy(gameObject);
+    private void Update()
+    {
+        // Calculate the distance traveled this frame and add it to the total.
+        float deltaDistance = speed * Time.deltaTime;
+        totalDistanceTraveled += deltaDistance;
+        transform.Translate(Vector3.up * deltaDistance);
+
+
+        if (totalDistanceTraveled > maxDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player")) return;
+        if (other.gameObject.CompareTag("Player"))
+            return;
 
-        if (other.gameObject.CompareTag("Asteroid2")) GameManager.instance.AddScore(1);
-        if (other.gameObject.CompareTag("Asteroid1")) GameManager.instance.AddScore(2);
+        if (other.gameObject.CompareTag("Asteroid2"))
+            GameManager.instance.AddScore(1);
+        if (other.gameObject.CompareTag("Asteroid1"))
+            GameManager.instance.AddScore(2);
+        
         Destroy(other.gameObject);
         Destroy(gameObject);
-
-       
     }
 }
